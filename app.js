e84fa6a3d7d46d6ca332d3c4f58ce1ff76f26d88
Jia Rong Tsai <jiarongtsai19@gmail.com>
2021-11-26 12:19:25 +0800

    執行standard JS 程式碼檢查

--- app.js
+++ app.js
@@ -4,41 +4,41 @@ const exphbs = require('express-handlebars')
 
 const restaurantList = require('./restaurant.json')
 
-//set engine 的方法容易忘記
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+// set engine 的方法容易忘記
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res)=>{
-    res.render('index', {restaurants: restaurantList.results })
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
 })
 
-app.get('/search', (req, res)=>{
-    //避免搜尋空白字串的情形
-    const keyword = req.query.keyword.trim()
-    const results = restaurantList.results.filter((restaurant)=>{
-        return keywordSearch(restaurant.name, keyword) || keywordSearch(restaurant.category, keyword)
-    })
+app.get('/search', (req, res) => {
+  // 避免搜尋空白字串的情形
+  const keyword = req.query.keyword.trim()
+  const results = restaurantList.results.filter((restaurant) => {
+    return keywordSearch(restaurant.name, keyword) || keywordSearch(restaurant.category, keyword)
+  })
 
-    // 新增獨立的查找函式，讓查找資料的程式碼簡化且能重複使用
-    function keywordSearch(string, keyword){
-        string = string.toLowerCase()
-        keyword = keyword.toLowerCase()
-        return string.includes(keyword)
-    }
+  // 新增獨立的查找函式，讓查找資料的程式碼簡化且能重複使用
+  function keywordSearch (string, keyword) {
+    string = string.toLowerCase()
+    keyword = keyword.toLowerCase()
+    return string.includes(keyword)
+  }
 
-    res.render('index', {restaurants: results, keyword })
+  res.render('index', { restaurants: results, keyword })
 })
 
-app.get('/restaurants/:id', (req, res)=>{
-    const restaurant = restaurantList.results.find((restaurant) => {
-        return restaurant.id.toString() === req.params.id
-    })
-    //the keyword "return" is indeed if ues ()=>{} sytax 
-    res.render('show', {restaurant})
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find((restaurant) => {
+    return restaurant.id.toString() === req.params.id
+  })
+  // the keyword "return" is indeed if ues ()=>{} sytax
+  res.render('show', { restaurant })
 })
 
-app.listen(3000, ()=>{
-    console.log('The server is listening on port 3000')
+app.listen(3000, () => {
+  console.log('The server is listening on port 3000')
 })
